432e78738afa691d19009666f0da79f7aab5cac7
pengzhanbo <volodymyr@foxmail.com>
2023-10-18 00:16:58 +0800

    feat: add useTitle

---
+++ src/useTitle/index.ts
@@ -0,0 +1,34 @@
+import { createSignal, onCleanup, onMount } from 'solid-js'
+import isBrowser from '../utils/isBrowser'
+
+export interface Options {
+  restoreOnUnmount?: boolean
+}
+
+const DEFAULT_OPTIONS: Options = {
+  restoreOnUnmount: false,
+}
+
+export default function useTitle(title: string, options = DEFAULT_OPTIONS) {
+  const defaultTitle = isBrowser ? document.title : ''
+  const [_title, _setTitle] = createSignal(defaultTitle)
+
+  onMount(() => {
+    document.title = title
+    _setTitle(title)
+    onCleanup(() => {
+      if (options.restoreOnUnmount) {
+        document.title = defaultTitle
+      }
+    })
+  })
+
+  const setTitle = (title: string) => {
+    if (isBrowser) {
+      document.title = title
+    }
+    setTitle(title)
+  }
+
+  return [_title, setTitle] as const
+}
