99a9322e9289054d4610a9217f176ecd31a476ef
pengzhanbo <volodymyr@foxmail.com>
2023-10-26 01:02:12 +0800

    docs: add `useEventListener` docs & demo

---
+++ src/useEventListener/demo/demo1.tsx
@@ -0,0 +1,29 @@
+/**
+ * title: Default usage
+ * desc: Click the button to preview.
+ *
+ * title.zh-CN: 基础用法
+ * desc.zh-CN: 点击按钮查看效果。
+ */
+
+import { useEventListener, useRef } from '@any-hooks/solid'
+import { createSignal } from 'solid-js'
+
+export default () => {
+  const [value, setValue] = createSignal(0)
+  const [ref, setRef] = useRef()
+
+  useEventListener(
+    'click',
+    () => {
+      setValue(value() + 1)
+    },
+    { target: ref },
+  )
+
+  return (
+    <button ref={setRef} type="button">
+      You click {value()} times
+    </button>
+  )
+}
---
+++ src/useEventListener/demo/demo2.tsx
@@ -0,0 +1,20 @@
+/**
+ * title: Listen keydown
+ * desc: Press any key to preview.
+ *
+ * title.zh-CN: 监听 keydown 事件
+ * desc.zh-CN: 按下键盘查看效果。
+ */
+
+import { useEventListener } from '@any-hooks/solid'
+import { createSignal } from 'solid-js'
+
+export default () => {
+  const [value, setValue] = createSignal('')
+
+  useEventListener('keydown', (ev) => {
+    setValue(ev.code)
+  })
+
+  return <p>Your press key is {value()}</p>
+}
