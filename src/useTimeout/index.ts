2bc0963460887c94110132bc68e16786879674da
pengzhanbo <volodymyr@foxmail.com>
2023-11-08 00:52:03 +0800

    feat: add `useTimeout` hooks

---
+++ src/useTimeout/index.ts
@@ -0,0 +1,53 @@
+import { type Accessor, createSignal, onCleanup, onMount } from 'solid-js'
+import { useWatch } from '..'
+import { isFunction, isNumber } from '../utils'
+
+/**
+ * A hook that executes a function after a specified delay.
+ *
+ * @param fn - The function to be executed.
+ * @param delay - The delay in milliseconds before executing the function.
+ *
+ * @example
+ * ```ts
+ * useTimeout(() => {
+ *   setState(state() + 1)
+ * }, 3000)
+ * ```
+ */
+export default function useTimeout(
+  fn: () => void,
+  delay?: number | (() => number | undefined) | Accessor<number | undefined>,
+) {
+  let timer: ReturnType<typeof setTimeout> | null = null
+  const [isMounted, setMounted] = createSignal(false)
+
+  const clear = () => timer && clearTimeout(timer)
+
+  const startTimeout = () => {
+    clear()
+    const _delay = isFunction(delay) ? delay() : delay
+    if (!isNumber(_delay) || _delay < 0) return
+    timer = setTimeout(fn, _delay)
+  }
+
+  onMount(() => {
+    setMounted(true)
+    startTimeout()
+    onCleanup(() => {
+      clear()
+      setMounted(false)
+    })
+  })
+
+  useWatch(
+    () => (isFunction(delay) ? delay() : delay),
+    () => {
+      if (isMounted()) {
+        startTimeout()
+      }
+    },
+  )
+
+  return clear
+}
