77da46b3e226a5b6123fd2f6da19d301e69d8073
pengzhanbo <volodymyr@foxmail.com>
2023-11-12 00:56:23 +0800

    feat: add `useThrottleSignal` hooks

---
+++ src/useThrottleSignal/index.ts
@@ -0,0 +1,26 @@
+import type { Setter } from 'solid-js'
+import { createSignal } from 'solid-js'
+import useThrottleFn from '../useThrottleFn'
+import type { ThrottleOptions } from '../useThrottleFn/throttleOptions'
+
+type ThrottleSetter<T> = Setter<T>
+
+/**
+ * A hook that deal with the throttled signal.
+ *
+ * 用来处理节流值的 Hook。
+ *
+ * @example
+ * ```ts
+ * const [state, setThrottledState, setSyncState] = useThrottleSignal(0, { wait: 500 })
+ * ```
+ */
+function useThrottleSignal<T>(value?: T, options?: ThrottleOptions) {
+  const [state, setState] = createSignal(value)
+
+  const { run } = useThrottleFn((value) => setState(value), options)
+
+  return [state, run as unknown as ThrottleSetter<T>, setState] as const
+}
+
+export default useThrottleSignal
