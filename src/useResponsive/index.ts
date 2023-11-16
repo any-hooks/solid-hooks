cbd9677a0de7a8ab0e2251be5cde16d12f206fdf
pengzhanbo <volodymyr@foxmail.com>
2023-11-17 00:47:07 +0800

    feat: add `useResponsive` hooks

---
+++ src/useResponsive/index.ts
@@ -0,0 +1,92 @@
+import { createSignal, onCleanup, onMount } from 'solid-js'
+import isBrowser from '../utils/isBrowser'
+
+type Subscriber = () => void
+
+const subscribers = new Set<Subscriber>()
+
+type ResponsiveConfig = Record<string, number>
+type ResponsiveInfo = Record<string, boolean>
+
+let info: ResponsiveInfo
+
+let responsiveConfig: ResponsiveConfig = {
+  xs: 0,
+  sm: 576,
+  md: 768,
+  lg: 992,
+  xl: 1200,
+}
+
+function handleResize() {
+  const oldInfo = info
+  calculate()
+  if (oldInfo === info) return
+  for (const subscriber of subscribers) {
+    subscriber()
+  }
+}
+
+let listening = false
+
+function calculate() {
+  const width = window.innerWidth
+  const newInfo = {} as ResponsiveInfo
+  let shouldUpdate = false
+  for (const key of Object.keys(responsiveConfig)) {
+    newInfo[key] = width >= responsiveConfig[key]
+    if (newInfo[key] !== info[key]) {
+      shouldUpdate = true
+    }
+  }
+  if (shouldUpdate) {
+    info = newInfo
+  }
+}
+
+export function configResponsive(config: ResponsiveConfig) {
+  responsiveConfig = config
+  if (info) calculate()
+}
+
+/**
+ * a Hook for getting responsive info.
+ *
+ * 获取响应式信息。
+ *
+ * @example
+ * ```ts
+ * const responsive = useResponsive()
+ * ```
+ */
+export function useResponsive() {
+  if (isBrowser && !listening) {
+    info = {}
+    calculate()
+    window.addEventListener('resize', handleResize)
+    listening = true
+  }
+  const [state, setState] = createSignal<ResponsiveInfo>(info)
+
+  onMount(() => {
+    if (!isBrowser) return
+
+    if (!listening) {
+      window.addEventListener('resize', handleResize)
+    }
+
+    const subscriber = () => setState(info)
+
+    subscribers.add(subscriber)
+
+    onCleanup(() => {
+      subscribers.delete(subscriber)
+      if (subscribers.size === 0) {
+        window.removeEventListener('resize', handleResize)
+        listening = false
+      }
+    })
+  })
+
+  return state
+}
